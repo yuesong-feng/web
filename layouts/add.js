c5dceff5903118e2b62749cf90c962da552ca3f4
yuesong-feng <yuesong-feng@foxmail.com>
2021-06-21 23:40:39 +0800

    update

--- layouts/add.js
+++ layouts/add.js
@@ -3,11 +3,24 @@ import React from "react";
 export default class Add extends React.Component {
   constructor(props) {
     super(props);
-    this.addArticle = this.addArticle.bind(this)
+    this.addArticle = this.addArticle.bind(this);
   }
-  addArticle(){
-    console.log('add');
-    
+  addArticle() {
+    fetch(
+      "https://service-afkj7z8q-1300477814.gz.apigw.tencentcs.com/release/add",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "添加文章测试",
+          author: "yuesong-feng",
+          content: "this is the content",
+        }),
+      }
+    )
+      .then((response) => console.log(response))
   }
   render() {
     return (
